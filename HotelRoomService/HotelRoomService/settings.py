2d9b2e98fe2606b4aecd2ab662f6ff8c3176ddab
Thiraphat_p <hone191@gmail.com>
2019-07-08 09:46:41 +0700

    Auto stash before revert of "[Red]Test1 ทดสอบการเข้าหน้าหลัก""

--- HotelRoomService/HotelRoomService/settings.py
+++ HotelRoomService/HotelRoomService/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'HotelRoomService.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['HotelRoomService/Hotelapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
