8d588ef684ea5bee5f8c916c396ce75ae71152d1
Thiraphat_p <hone191@gmail.com>
2019-07-09 18:02:15 +0700

    fix model

--- HotelRoomService/Hotelapp/models.py
+++ HotelRoomService/Hotelapp/models.py
@@ -7,9 +7,8 @@ from django.db import models
 class Room(models.Model):
     name = models.CharField(max_length=250)
     des = models.CharField(max_length=250)
-    option = models.CharField(max_length=250)
-    address = models.CharField(max_length=250)
     peple = models.IntegerField(default=1)
+    price = models.CharField(max_length=250)
     image1 = models.ImageField(upload_to='imageroom', blank=True, null=True)
     image2 = models.ImageField(upload_to='imageroom', blank=True, null=True)
     image3 = models.ImageField(upload_to='imageroom', blank=True, null=True)
@@ -25,3 +24,28 @@ class Room(models.Model):
     def __str__(self):
         return '{}'.format(self.name)
 
+
+class Option(models.Model):
+    optionname = models.CharField(max_length=250)
+
+
+    class Meta:
+        ordering= ('id',)
+        verbose_name ='option'
+        verbose_name_plural='options'
+
+    def __str__(self):
+        return '%s' % (self.optionname)
+
+class RoomOption(models.Model):
+    name = models.ForeignKey(Room, null=False,  on_delete=models.CASCADE)
+    option = models.ForeignKey(Option, null=False,  on_delete=models.CASCADE)
+
+
+    class Meta:
+        ordering= ('id',)
+        verbose_name ='roomoption'
+        verbose_name_plural='roomoptions'
+
+    def __str__(self):
+        return '%s' % (self.name)
