0a3e8167f731d83f5f9a5f050c13037aa9074cf7
Thiraphat_p <hone191@gmail.com>
2019-07-09 16:00:34 +0700

    [Refactor][ห้องพัก]หน้าห้องพัก
    แก้การแสดงผลห้อง เพิ่มโมเดล ลิ้ง url

--- HotelRoomService/HotelRoomService/views.py
+++ HotelRoomService/HotelRoomService/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from Hotelapp.models import Room 
 
 
 # Create your views here.
@@ -13,9 +13,14 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 def room(request):
-    return render(request,'Roompage.html')
-def roomdetail(request):
-    return render(request,'Roomdetail.html')
+
+    room = Room.objects.all()
+    
+    return render(request,'Roompage.html',{'room':room})
+def roomdetail(request,room_id):
+    room = Room.objects.get(id=room_id)  
+
+    return render(request,'Roomdetail.html',{'room':room})
 def fitness(request):
     return render(request,'Fitness.html')
 def spa(request):
