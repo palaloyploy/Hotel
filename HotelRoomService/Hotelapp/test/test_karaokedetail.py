e3e37b90eba6397a2bf934111fac7b141d1dedb7
palaloyploy <palaloy_pp@hotmail.com>
2019-07-15 17:30:36 +0700

    [Green][ห้องคาราโอเกะ]test5 ทดสอบการจองไม่สำเร็จหากไม่กรอกชื่อ-นามสกุล เพียงอย่างเดียว

--- HotelRoomService/Hotelapp/test/test_karaokedetail.py
+++ HotelRoomService/Hotelapp/test/test_karaokedetail.py
@@ -15,12 +15,12 @@ class KaraokeDetailViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'Hotelapp/karaokedetail.html')
 
-    # def test_view_boatdetail_GET_list_is_1item(self):
+    def test_view_karaokedetailGET_list_is_1item(self):
         
-    #     response = self.client.get(reverse('boatdetail', kwargs={'boat_id':1}))
+        response = self.client.get(reverse('karaokedetail', kwargs={'karaoke_id':1}))
 
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.context['boat'].id,1)  
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['karaoke'].id,1)  
 
     # def test_view_boatdetail_GET_list_is_1item(self):
         
